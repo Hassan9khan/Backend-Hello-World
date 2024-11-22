9df71639431ebf5bc9ad79b1a5c580108477bb61
Hassan Ahmed <hassanshakeel381@gmail.com>
2024-11-22 23:12:59 +0500

    updated

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.get('/' , (req , res) => {
 app.get('/about' , (req , res) => {
   res.send('Google Calendar')
 })
+app.post('/contact' , (req , res) => {
+  res.send('Google Calendar')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
